3b5c7befea46e8b4a23dd7ae019416c1c4f0b312
shubham joshi <shubham.joshi@successive.tech>
2019-11-25 13:00:49 +0530

    removed all console logs

--- src/resolvers/resolvers.ts
+++ src/resolvers/resolvers.ts
@@ -16,7 +16,6 @@ export const resolvers = {
 
   Mutation: {
     createUser: async (_, { name, lastName, email, password, phonenumber, brandname, streetaddress, city, country, postalcode }, { dataSources }) => {
-      console.log(dataSources);
       const res = await dataSources.user.createUser({ name, lastName, email, password, phonenumber, brandname, streetaddress, city, country, postalcode });
       await pubsub.publish(user_Added, { userCreated: res.data });
       return res.data;
